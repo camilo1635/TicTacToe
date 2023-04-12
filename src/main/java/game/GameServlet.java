4ca8c77fa1382abc3254cc19eba35b501a582302
camilo <jcorreac@unicauca.edu.co>
2023-04-12 17:57:16 -0500

    Actualizacion a idioma español

--- src/main/java/game/GameServlet.java
+++ src/main/java/game/GameServlet.java
@@ -37,23 +37,23 @@ public class GameServlet extends HttpServlet {
 				case NOBODY:
 					break;
 				case COMPUTER:
-					request.setAttribute("winner", "The computer");
+					request.setAttribute("winner", "La maquina");
 					break;
 				case USER:
-					request.setAttribute("winner", "You");
+					request.setAttribute("winner", "Tu");
 					break;
 				}
 			}
 			break;
 		case COMPUTER:
-			request.setAttribute("winner", "The computer");
+			request.setAttribute("winner", "La maquina");
 			break;
 		case USER:
-			request.setAttribute("winner", "You");
+			request.setAttribute("winner", "Tu");
 			break;
 	}
 	if(winner == GamePlayer.NOBODY && !game.hasEmptyCell()){
-		request.setAttribute("winner", "Nobody");
+		request.setAttribute("winner", "Nadie");
 	}
 	request.getRequestDispatcher(";/game.jsp").forward(request, response);
 	}
